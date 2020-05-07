c8aaeaa2be661491107c1a8d145e983d44f8ef06
Daniel <fraxdev@gmail.com>
2020-05-07 20:58:42 +0300

    c

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, ViewChild } from '@angular/core';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild(DashboardComponent) dashboard: DashboardComponent;
+
+  title = 'app';
+
+  refresh() {
+    this.dashboard.generateData();
+  }
+}
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  @Output() onRefresh: EventEmitter<null> = new EventEmitter<null>();
+
+  constructor() { }
+
+  ngOnInit() {
+    this.onRefresh.emit();
+  }
+
+}
